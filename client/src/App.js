a23d8bcf95a25fd2d3b6f08c3196ea68c09ae641
eaguirrebayli-at-858853655499 <emi.aguirre@hotmail.com>
2023-05-29 12:31:46 -0600

    Initial game frontend logic

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import GameBoard from './Components/GameBoard.jsx'
+import Score from './Components/Score';
+import Lives from './Components/Lives';
+
+function App() {
+  return (
+      <div>
+        <GameBoard />
+      </div>
+  );
+}
+
+export default App;
